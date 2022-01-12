f6741a7a595e07ddfbed843dab5d4ed624eb56b8
Haleem Hosein <haleemhosein@yahoo.com>
2022-01-12 18:20:02 -0400

    02 application prep work - make Image model, view, controller and migration

---
+++ resources/views/images/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Image Uploading with Vue + Laravel</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="max-w-lg mx-auto mt-24">
+    <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+</div>
+</body>
+
+</html>
